db2ea20640da98e1f58fa46f290ef4fcdbf3c249
Gabriel Nierich <131723437+Nierich@users.noreply.github.com>
2025-03-21 02:30:00 +0000

    mudando a frase de novo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Obrigado Por visitar meu primeiro deploy!!</h1>;
+    return <h1>Lari, obrigado por ser minha motivação todos os dias!</h1>;
 }
 
 export default Home;
